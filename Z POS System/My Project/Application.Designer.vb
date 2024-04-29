fd7867c9855c209384c58842ecd5776155da0c7d
zylrey <69803117+zylrey@users.noreply.github.com>
2024-04-29 09:04:27 +0800

    Folders
    Secondary Commit

--- Z POS System/My Project/Application.Designer.vb
+++ Z POS System/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Z_POS_System.Form1
+			Me.MainForm = Global.Z_POS_System.frmMain
         End Sub
     End Class
 End Namespace
